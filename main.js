df7c54cce346caf97fe5b7aead839abd183a9d9d
YT330113 <93573775+YT330113@users.noreply.github.com>
2023-03-19 19:32:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function setUserName() {
       setUserName();
     } else {
       localStorage.setItem('name', myName);
-      myHeading.textContent = '蛮球 酷毙了，' + myName;
+      myHeading.textContent = '爱蛮球 酷毙了，' + myName;
     }
   }
   
